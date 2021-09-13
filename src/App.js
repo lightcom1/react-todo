2ee44593e245783bf47cc54b72aa5460756c6754
Denys Kurylenko <lightcom2001@gmail.com>
2021-09-13 18:31:02 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { v4 as uuid } from 'uuid';
+import { randomColor } from 'randomcolor';
+import Draggable from 'react-draggable';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [item, setItem] = useState('');
+	const [items, setItems] = useState(
+		JSON.parse(localStorage.getItem('items')) || []
+	);
+
+	useEffect(() => {
+		localStorage.setItem('items', JSON.stringify(items));
+	}, [items]);
+
+	const newItem = (e = null) => {
+		if (item.trim() !== '') {
+			const newItem = {
+				id: uuid(),
+				item,
+				color: randomColor({
+					luminosity: 'light',
+				}),
+				defaultPos: {
+					x: 50,
+					y: -320,
+				},
+			};
+			setItems(items => [...items, newItem]);
+			setItem('');
+		} else {
+			if (e)
+				e.target.previousElementSibling.style.borderBottom = '1px solid red';
+			setItem('');
+		}
+	};
+
+	const deleteNote = id => {
+		setItems(items.filter(item => item.id !== id));
+	};
+
+	const updatePos = (data, i) => {
+		let newArr = [...items];
+		newArr[i].defaultPos = { x: data.x, y: data.y };
+		setItems(newArr);
+	};
+
+	const keyPress = e => {
+		const code = e.keyCode || e.which;
+		if (code === 13) {
+			newItem();
+			if (!e.target.value) e.target.style.borderBottom = '1px solid red';
+		}
+	};
+
+	const check = e => {
+		if (!e.target.value) {
+			e.target.style.borderBottom = '1px solid red';
+		} else {
+			e.target.style.borderBottom = '1px solid whitesmoke';
+		}
+	};
+
+	return (
+		<div className='App'>
+			<div className='wrapper'>
+				<input
+					value={item}
+					type='text'
+					placeholder='Enter smth'
+					onChange={e => setItem(e.target.value)}
+					onKeyPress={e => keyPress(e)}
+					onInput={e => check(e)}
+				/>
+				<button className='enter' onClick={e => newItem(e)}>
+					ENTER
+				</button>
+			</div>
+
+			{items.map((item, i) => {
+				return (
+					<Draggable
+						key={i}
+						defaultPosition={item.defaultPos}
+						onStop={(_, data) => updatePos(data, i)}>
+						<div className='todo__item' style={{ backgroundColor: item.color }}>
+							{`${item.item}`}
+							<button className='delete' onClick={() => deleteNote(item.id)}>
+								&times;
+							</button>
+						</div>
+					</Draggable>
+				);
+			})}
+		</div>
+	);
 }
 
 export default App;
